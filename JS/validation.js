093ca1b61e6856abc4198215242d986c31b7dab9
jul23vamshi1 <vamshi.hy311@gmail.com>
2016-04-10 14:29:00 -0500

    My First Website With JavaScript

---
+++ JS/validation.js
@@ -0,0 +1,6 @@
+function contact() {
+	var firstName = document.getElementById("firstName");
+	var lastName = document.getElementById("lastName").value;
+	window.alert('Thanks for sharing the details '+lastName +'. We will get back to you Shortly');
+	
+}
